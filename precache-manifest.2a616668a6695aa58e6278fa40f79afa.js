8ab429ae8c84d236bb9e624618f71eb950d64d75
Pavlo Dudar <dudar.pavloGmail.com>
2019-11-28 20:34:32 +0200

    Updates

---
+++ precache-manifest.2a616668a6695aa58e6278fa40f79afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e236c56fb30b1451e6ec3dbc8f4d7d",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "3ac89cdd1e049ec460e3",
+    "url": "/weather/static/css/main.3e277969.chunk.css"
+  },
+  {
+    "revision": "2c6f493895a36fdb9b59",
+    "url": "/weather/static/js/2.fab2920f.chunk.js"
+  },
+  {
+    "revision": "3ac89cdd1e049ec460e3",
+    "url": "/weather/static/js/main.92dd57c1.chunk.js"
+  },
+  {
+    "revision": "145cd1151c9280c8a6ca",
+    "url": "/weather/static/js/runtime-main.a0e9584a.js"
+  },
+  {
+    "revision": "c8e63b3a2fd1f852437ba343991a97be",
+    "url": "/weather/static/media/cloud.c8e63b3a.png"
+  },
+  {
+    "revision": "e89323b7f0d288934d63074bfa4e5fdb",
+    "url": "/weather/static/media/map3.e89323b7.png"
+  }
+]);
